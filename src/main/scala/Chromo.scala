8490cca7fc4330ac3e1d3ec2e0a79a30b9473836
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-04-19 13:45:17 +0100

    finish solution using splitat.

--- src/main/scala/Chromo.scala
+++ src/main/scala/Chromo.scala
@@ -2,12 +2,17 @@ object Chromo extends App {
 
   def swap(input1: String, input2: String, index : Int): List[String] = {
 
-    val stringLength = input1.length-index
+//    val stringLength = input1.length-index
+//
+//    val input1new = input1.take(stringLength).concat(input2.takeRight(index))
+//    val input2new = input2.take(stringLength).concat(input1.takeRight(index))
+//
+//    List(input1new,input2new)
 
-    val input1new = input1.take(stringLength).concat(input2.takeRight(index))
-    val input2new = input2.take(stringLength).concat(input1.takeRight(index))
+    val split1 = input1.splitAt(index)
+    val split2 = input2.splitAt(index)
 
-    List(input1new,input2new)
+    List(split1._1 + split2._2, split2._1 + split1._2)
 
   }
 
---
+++ src/main/scala/Help.sc
@@ -0,0 +1,19 @@
+val string = "Hello"
+
+string.take(1)
+
+val list1 = "00000"
+val list2 = "11111"
+
+list1.take(2)
+list2.takeRight(2)
+val concat = list1.take(3).concat(list2.takeRight(3))
+
+list1.splitAt(0)
+list1.splitAt(2)
+list1.splitAt(4)
+list1.splitAt(5)
+
+list1.substring(0,2)
+list1.substring(0)
+list1.substring(4)
